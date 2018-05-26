c7fe1e04f8da8ef66fbfc01d26fa488533ce678d
MaximilianoCabrera <maximilianocabrerap@gmail.com>
2018-05-26 18:49:18 -0300

    Initial commit

---
+++ application/views/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" lang="es-ES" prefix="og: http://ogp.me/ns#">
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" lang="es-ES" prefix="og: http://ogp.me/ns#">
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+
+<html lang="es">
+
+<!--<![endif]-->
+
+<head>
+	<link rel="stylesheet" href="../../assets/css/style.css" type='text/css'/>
+</head>
+
+<body class="home" >
+
+<header>
+    Cabecera
+</header>
+<div style="clear:both"></div>
